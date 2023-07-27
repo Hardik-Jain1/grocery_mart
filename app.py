2d147c851ea489bdf02d8f133aed138bc837a61b
Hardik <jnharsh121@gmail.com>
2023-07-27 20:03:36 +0530

    created templates and added user registeration method

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, PasswordField, SubmitField
-# from wtforms.validators import InputRequired, Length, ValidationError
-# from flask_bcrypt import Bcrypt
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import  bcrypt
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.sqlite3"
+app.config['SECRET_KEY'] = 'thisisasecretkey'
 db = SQLAlchemy()
 db.init_app(app)
 app.app_context().push()
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Users(UserMixin, db.Model):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_name = db.Column(db.String(200), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.String(200), nullable=False)
 
 class Sections(db.Model):
     section_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -34,3 +45,59 @@ class Products(db.Model):
     section_id = db.Column(db.Integer, db.ForeignKey('sections.section_id'))
     section = db.relationship('Sections', backref=db.backref('products', lazy=True))
 
+db.create_all()
+    
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(max=200)], render_kw={"placeholder": "Username"})
+    
+    role = StringField(validators=[
+                    InputRequired(), Length(max=200)], render_kw={"placeholder": "Role"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=200)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = Users.query.filter_by(
+            user_name=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+        
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/user_login")
+def user_login():
+    return render_template("user_login.html")
+
+@app.route("/user_register", methods=['GET', 'POST'])
+def user_register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.hashpw(form.password.data.encode('utf-8'), bcrypt.gensalt())
+        new_user = Users(user_name=form.username.data, role=form.role.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('user_login'))
+
+    return render_template('user_register.html', form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
