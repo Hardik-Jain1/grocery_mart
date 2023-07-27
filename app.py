f7c72352df7e2771a9c3904eabb5065c16ab8872
Hardik <jnharsh121@gmail.com>
2023-07-27 18:30:43 +0530

    added database

--- app.py
+++ app.py
@@ -1,2 +1,36 @@
-#My new file
-print("First Github-VSCode programe")
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, SubmitField
+# from wtforms.validators import InputRequired, Length, ValidationError
+# from flask_bcrypt import Bcrypt
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.sqlite3"
+db = SQLAlchemy()
+db.init_app(app)
+app.app_context().push()
+
+
+class Users(UserMixin, db.Model):
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+
+class Sections(db.Model):
+    section_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    section_name = db.Column(db.String(200), nullable=False)
+    number_of_products = db.Column(db.Integer)
+
+class Products(db.Model):
+    product_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_name = db.Column(db.String(200), nullable=False)
+    manufacture_date = db.Column(db.Date)
+    expiry_date = db.Column(db.Date)
+    rate_per_unit = db.Column(db.Float, nullable=False)
+    section_id = db.Column(db.Integer, db.ForeignKey('sections.section_id'))
+    section = db.relationship('Sections', backref=db.backref('products', lazy=True))
+
